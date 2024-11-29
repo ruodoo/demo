205cd251ee18de4f1e1eccc17103881d2ccccb39
Nini1551 <47613768+Nini1551@users.noreply.github.com>
2024-11-29 16:14:08 +0100

    [FIX] : print the user description

--- demo/models/models.py
+++ demo/models/models.py
@@ -16,6 +16,7 @@ class User(models.Model):
     @api.constrains('description')
     def _description_is_one_line(self):
         for user in self:
+            print(user.description)
             if '\n' in user.description:
                 raise ValueError(f'Description must be oneline, got `{user.description}`')
 
