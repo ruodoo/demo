05ea13f18bfe58f84c19b65c56e09332fb2deb36
GuillaumeDelferiere <guigui.delferiere@gmail.com>
2024-11-29 16:23:25 +0100

    [FIX] correction du test

--- demo/tests/test_models.py
+++ demo/tests/test_models.py
@@ -23,11 +23,12 @@ class TestUsers(TransactionCase):
         self.env.flush_all()
 
     def test_description_is_required(self):
-        user = self.env['res.users'].create({
-                'name': 'Marie-Noël',
-                'login': 'mnv',
-            })
-        user.description = None
+        with self.assertRaises(NotNullViolation) :
+            user = self.env['res.users'].create({
+                    'name': 'Marie-Noël',
+                    'login': 'mnv',
+                })
+            user.description = None
 
 
     def test_description_one_line(self):
